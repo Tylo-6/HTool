5a899b6b0008a7796c50f1f7f4ccdb2db450214d
Tylo-6 <104464015+Tylo-6@users.noreply.github.com>
2022-04-27 07:38:47 -0400

    Update core.js

--- src/core.js
+++ src/core.js
@@ -1,13 +1,14 @@
+console.log("opened 1/2");
 // source
 window.hax.start = function() {
   // open new window
   // ...
   // window.open(window.URL,"Ratting","width=550,height=170,left=150,top=200,toolbar=0,status=0,");
-  print("working");
+  console.log("working");
   // inject code into window
   // ...
 }
 // functions
 // ...
-
+console.log("opened 2/2")
 hax.start();
