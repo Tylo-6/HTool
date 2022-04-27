5c60ce886f4868d57ea49789bb6500470cd1b051
Tylo-6 <104464015+Tylo-6@users.noreply.github.com>
2022-04-27 09:39:39 -0400

    Update core.js

--- src/core.js
+++ src/core.js
@@ -1,3 +1,4 @@
+alert("ran");
 function haxtoolcreate() {
   alert("creating window");
   let win = window.open('',"Ratting","width=550px,height=350px,left=150,top=200,toolbar=0,status=0,"); req = new XMLHttpRequest(); req.open('GET', 'https://raw.githubusercontent.com/Tylo-6/HTool/main/src/window.html'); req.onload = function() { win.document.write(this.responseText); }; req.send();
